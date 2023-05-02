64107fbb394e3b586e07159716c9d9ffa9280fee
olita16 <olenka161991@gmail.com>
2023-05-02 15:57:36 +0200

    first commit hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+const galleryList = document.querySelector('.gallery');
+
+export function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+ <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>
+ `;
+    })
+    .join('');
+}
+
+const galleryMarkup = createGalleryItem(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryList.addEventListener('click', onGaleryItemsClick);
+
+function onGaleryItemsClick(e) {
+  e.preventDefault();
+}
+
+const gallery = new SimpleLightbox('.gallery__item > .gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  fadeSpeed: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const THROTTLE_DELAY = 1000;
+
+const saveCurrentTime = throttle(currentTime => {
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, THROTTLE_DELAY);
+
+const getSavedTime = () => {
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  return savedTime ? parseFloat(savedTime) : 0;
+};
+
+player.setCurrentTime(getSavedTime());
+
+player.on('timeupdate', ({ seconds }) => {
+  saveCurrentTime(seconds);
+});
